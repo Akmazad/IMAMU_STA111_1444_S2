adc6661453df384c92838d84bd5098fe997d9119
A K M AZAD <azad_du28@yahoo.com>
2022-12-11 21:04:06 +0300

    Update class codes.R

--- chapter1/class codes.R
+++ chapter1/class codes.R
@@ -53,5 +53,5 @@ dat <- data.frame(
   housePrice = c(245,312,279,308,199,219,405,324,319,255),
   sqFeet = c(1400,1600,1700,1875,1100,1550,2350,2450,1425,1700)
 )
-temp = lm(housePrice ~ sqFeet, data = dat)
-summary(temp)
+fit = lm(housePrice ~ sqFeet, data = dat)
+summary(fit)
